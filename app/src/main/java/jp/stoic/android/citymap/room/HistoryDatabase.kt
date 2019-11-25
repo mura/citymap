929ed286e4e208a80fdb9f7d1edc18899fb8c06d
Yohei Murayama <muracchi@gmail.com>
2019-11-25 10:40:00 +0900

    ライブラリのアップデート

--- app/src/main/java/jp/stoic/android/citymap/room/HistoryDatabase.kt
+++ app/src/main/java/jp/stoic/android/citymap/room/HistoryDatabase.kt
@@ -3,7 +3,7 @@ package jp.stoic.android.citymap.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [History::class], version = 1)
+@Database(entities = [History::class], version = 1, exportSchema = false)
 abstract class HistoryDatabase : RoomDatabase() {
     abstract fun historyDao(): HistoryDao
 }
